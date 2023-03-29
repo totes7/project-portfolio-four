ad5efbb3e4a75e2ba600ba55349b4fff4bb9fd1b
totes7 <antonio.manno.7@gmail.com>
2023-03-29 17:31:25 +0000

    Add dropdown menu for booking in navbar, so that user can choose to go to mybooking page or to makebooking page

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // dropdown initialization
+    let dropdown = document.querySelectorAll('.dropdown-trigger');
+    M.Dropdown.init(dropdown);
+
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
